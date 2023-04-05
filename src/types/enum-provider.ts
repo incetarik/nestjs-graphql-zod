c2966adaeb9c718ba8565888cc647b2da086d520
Tarık İnce <incetarik@hotmail.com>
2023-04-05 13:46:00 +0300

    Added `EnumProvider` function type

---
+++ src/types/enum-provider.ts
@@ -0,0 +1,50 @@
+/**
+ * The enum provider data.
+ */
+export interface EnumProviderData {
+  /**
+   * The property name of the enum.
+   *
+   * @type {string}
+   * @memberof EnumProviderData
+   */
+  name: string
+
+  /**
+   * The parent name that contains the enum type.
+   *
+   * @type {string}
+   * @memberof EnumProviderData
+   */
+  parentName?: string
+
+  /**
+   * The description of the enum.
+   *
+   * @type {string}
+   * @memberof EnumProviderData
+   */
+  description?: string
+}
+
+/**
+ * Gets an enum type for given information.
+ *
+ * Use this function to prevent creating different enums in GraphQL schema
+ * if you are going to use same values in different places.
+ *
+ * @param {(Record<string, string | number>)} enumObject The enum object
+ * that is extracted from the zod.
+ *
+ * @param {EnumProviderData} info The information of the enum property.
+ *
+ * @return {(Record<string, string | number> | undefined)} The enum
+ * that will be used instead of creating a new one. If `undefined` is
+ * returned, then a new enum will be created.
+ *
+ * @memberof IModelFromZodOptions
+ */
+export type EnumProvider = (
+  enumObject: object,
+  info: EnumProviderData
+) => object | undefined
